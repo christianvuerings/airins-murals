96919daa9008e2cf41495e137e0497210a14dd8d
Christian Vuerings <cvuerings@pinterest.com>
2019-07-07 18:11:57 -0700

    CSS fixes

--- src/js/cms.js
+++ src/js/cms.js
@@ -5,18 +5,19 @@ import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
-
+import AboutPreview from "./cms-preview-templates/contact";
 
 // Example of creating a custom color widget
 class ColorControl extends React.Component {
   render() {
-    return <input
-      style={{height: "80px"}}
-      type="color"
-      value={this.props.value}
-      onInput={(e) => this.props.onChange(e.target.value)}
-    />;
+    return (
+      <input
+        style={{ height: "80px" }}
+        type="color"
+        value={this.props.value}
+        onInput={e => this.props.onChange(e.target.value)}
+      />
+    );
   }
 }
 
@@ -25,5 +26,5 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("about", AboutPreview);
 CMS.registerWidget("color", ColorControl);
